bbe0213ef0ea097837b53396b5364f30016cc977
Pablo-RoVi <pablo.robledo@alumnos.ucn.cl>
2024-05-20 17:24:41 -0400

    refactor: update statistics graphs

--- app/Statistics.py
+++ app/Statistics.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
 
 file_name_main = ''
 
@@ -11,30 +10,35 @@ def read_coordinates(filename):
 def show_histogram(scenario_df, file):
     scenario_df['Ley_Metal'] = scenario_df['Metal'] / scenario_df['Tonelaje']
 
-    plt.figure(figsize=(10, 6))
+    plt.subplot(2, 1, 1)
     plt.hist(scenario_df['Ley_Metal'], bins=10, edgecolor='black')
     plt.title(f'Histograma de Leyes de Metal: {file}')
     plt.xlabel('Ley de metal')
     plt.ylabel('Frecuencia')
     plt.grid(True)
-    plt.show()
 
 def show_curve(scenario_df, file):
-    scenario_sorted = scenario_df.sort_values(by='Ley_Metal')
+    scenario_df = scenario_df.sort_values(by='Ley_Metal', ascending=False)
 
-    plt.figure(figsize=(10, 6))
-    plt.plot(scenario_sorted['Ley_Metal'], scenario_sorted['Tonelaje'], marker='o')
+    scenario_df = scenario_df.reset_index(drop=True)
+
+    plt.subplot(2, 1, 2)
+    plt.plot(scenario_df['Ley_Metal'])
     plt.title(f'Curva de tonelaje vs ley de metal: {file}')
-    plt.xlabel('Ley de metal')
-    plt.ylabel('Tonelaje')
+    plt.ylabel('Ley de metal')
     plt.grid(True)
-    plt.show()
 
 def show_scenario_statistics(file_name):
     file_name_main = file_name
     coordinates_df = read_coordinates(file_name)
+
+    plt.figure(figsize=(10, 6))
+
     show_histogram(coordinates_df, file_name)
     show_curve(coordinates_df, file_name)
+
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == "__main__":
     show_scenario_statistics(file_name_main)
