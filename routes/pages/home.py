b794d41f1733e5012ed317815e9f152edb3f1206
mizok <cairamgo@gmail.com>
2024-05-19 23:01:45 -0400

    feat: added github icon on footer

--- routes/pages/home.py
+++ routes/pages/home.py
@@ -19,6 +19,8 @@ def home_page():
     with ui.footer().classes('flex justify-end items-end text-end text-xs mt-5 bg-transparent text-white font-bold'):
         ui.label('© Minero Pro')
         ui.label('|')
+        ui.image('assets/github-icon.svg').classes('w-[16px] h-[16px] hover:opacity-80 cursor-pointer')
+        ui.label('|')
         ui.link('David Araya', 'https://github.com/Dizkm8')
         ui.link('Marcelo Céspedes', 'https://github.com/ZenkaiRed')
         ui.link('Carlo Ramírez', 'https://github.com/mizook')
