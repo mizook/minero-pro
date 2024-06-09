a88e5eec6ece1d83c7299a3df4fc8f16ec444a3f
Pablo-RoVi <pablo.robledo@alumnos.ucn.cl>
2024-06-09 18:06:29 -0400

    fix: using function from utils

--- utils/global_functions.py
+++ utils/global_functions.py
@@ -1,7 +1,9 @@
 import pandas as pd
+from utils.utils import Utils as utl
 
 
 def read_coordinates(file_name):
-    df = pd.read_csv(f'data/scenarios/{file_name}', header=None,
+    path = utl.get_resource_path(f"data/scenarios/{file_name}")
+    df = pd.read_csv(path, header=None,
                      names=["x", "y", "z", "total_tonn", "gold_tonn", "else_tonn"])
     return df
