8765ea5a2d2a09b7c0d08a7a010c33ee0aead263
Pablo-RoVi <pablo.robledo@alumnos.ucn.cl>
2024-06-16 20:35:47 -0400

    fix: tight_layout warning

--- app/statistics.py
+++ app/statistics.py
@@ -4,10 +4,10 @@ import re
 
 # Mostrar histograma
 def show_histogram(ax, scenario_df, file_number):
-    # Calcula la ley de metal de cada cubo
+    # Calcular la ley de metal de cada cubo
     scenario_df['metal_law_gold'] = scenario_df['gold_tonn'] / scenario_df['total_tonn']
     
-    # Crea un histograma
+    # Crear un histograma
     ax.hist(scenario_df['metal_law_gold'], bins=10, edgecolor='black')
     ax.set_title(f'Histograma de Leyes de Metal: Escenario {file_number}')
     ax.set_xlabel('Ley de metal')
@@ -16,14 +16,14 @@ def show_histogram(ax, scenario_df, file_number):
 
 # Mostrar curva
 def show_curve(ax, scenario_df, file_number):
-    # Ordena el DataFrame por ley de metal
+    # Ordenar el DataFrame por ley de metal
     scenario_df = scenario_df.sort_values(by='metal_law_gold', ascending=False)
     
-    # Calcula el tonelaje acumulado y la ley media
+    # Calcular el tonelaje acumulado y la ley media
     scenario_df['cumulative_tonn'] = scenario_df['total_tonn'].cumsum()
     scenario_df['average_metal_law'] = scenario_df['gold_tonn'].cumsum() / scenario_df['cumulative_tonn']
     
-    # Reinicia los índices
+    # Reiniciar los índices
     scenario_df = scenario_df.reset_index(drop=True)
     
     # Primer eje Y (izquierdo) - Tonelaje acumulado
@@ -39,7 +39,7 @@ def show_curve(ax, scenario_df, file_number):
     ax2.set_ylabel('Ley media', color='r')
     ax2.tick_params(axis='y', labelcolor='r')
 
-    # Combina las leyendas de ambos ejes
+    # Combinar las leyendas de ambos ejes
     lines, labels = ax.get_legend_handles_labels()
     lines2, labels2 = ax2.get_legend_handles_labels()
     ax.legend(lines + lines2, labels + labels2, loc='best')
@@ -49,23 +49,20 @@ def show_curve(ax, scenario_df, file_number):
 
 # Mostrar estadísticas de un escenario
 def show_scenario_statistics(file_name):
-    # Obtiene el número de escenario
+    # Obtener el número de escenario
     file_number = int(re.search(r'\d+', file_name).group()) + 1
     
-    # Lee las coordenadas del archivo
+    # Leer las coordenadas del archivo
     coordinates_df = read_coordinates(file_name)
     
-     # Crea una figura con dos subplots
+     # Crear una figura con dos subplots
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6), constrained_layout=True)
     
-    # Muestra el histograma
+    # Crear el histograma
     show_histogram(ax1, coordinates_df, file_number)
     
-    # Muestra la curva
+    # Crear la curva
     show_curve(ax2, coordinates_df, file_number)
     
-    # Ajusta el diseño
-    fig.tight_layout()
-    
-    # Muestra la figura
+    # Mostrar la figura
     plt.show()
