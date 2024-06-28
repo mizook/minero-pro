3e5c12dc1111aa7eac4db8e0d2194f7cdfe74c68
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2024-06-28 09:42:45 -0400

    feat: change 2d view to slices because of better requirement understanding

--- app/modeler.py
+++ app/modeler.py
@@ -43,9 +43,28 @@ def rendering_2d(file_name: str, title: str, axis_view: str, event):
     event.set()
 
 
+def rendering_slice_2d(
+    file_name: str, title: str, axis_view: str, event, slice_x: float = None
+):
+    grid, plotter = abstract_rendering(file_name)
+
+    sliced_mesh = grid.slice(normal="x")
+
+    plotter.add_mesh(sliced_mesh, color="orange", show_edges=True)
+    plotter.show_axes()
+
+    plotter.title = title
+
+    thread = threading.Thread(target=PlotCommon.bring_window_to_front, args=(title,))
+    thread.start()
+
+    plotter.show()
+    event.set()
+
+
 def open_2d_scenery(file_name: str, title: str, axis_view: str, event):
     process = multiprocessing.Process(
-        target=rendering_2d, args=(file_name, title, axis_view, event)
+        target=rendering_slice_2d, args=(file_name, title, axis_view, event, 10)
     )
     process.start()
 
